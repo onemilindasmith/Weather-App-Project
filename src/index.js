1534fd87381937828d2634f5625db9bb4128e0a4
onemilindasmith <120619288+onemilindasmith@users.noreply.github.com>
2023-02-21 02:37:37 -0500

    Updated date & time JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let day = date.getDay();
+    return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response){
     console.log(response.data);
     let temperatureElement = document.querySelector("#real-Temp");
@@ -5,12 +13,14 @@ function displayTemperature(response){
     let windElement = document.querySelector("#Wind");
     let humididtyElement = document.querySelector("#Humidity");
     let descriptionelement = document.querySelector("#description");
+    let dateElement = document.querySelector("#currently");
     temperatureElement.innerHTML = Math.round(response.data.main.temp) + "°F";
     cityElement.innerHTML = response.data.name;
     windElement.innerHTML = ("Wind Speed: " + (response.data.wind.speed) + "mph");
     humididtyElement.innerHTML =
       "Humidity: " + (response.data.main.humidity) + "%";
       descriptionelement.innerHTML = response.data.weather[0].description;
+      dateElement.innerHTML = formatDate(response.data.dt * 1000); 
 }
 
 
