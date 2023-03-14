772e116f08b8b8495db2574a3555beed14ab1f8c
onemilindasmith <120619288+onemilindasmith@users.noreply.github.com>
2023-03-13 22:43:08 -0400

    JS Update
    -removed 5day forecast code from HTML and moved it to JS
    -created a new function, variable, & looped the days
    -wrote HTML inside JS

--- src/index.js
+++ src/index.js
@@ -13,6 +13,37 @@ function formatDate(timestamp){
     return `${day} ${hours}:${minutes}`;
 }
 
+
+function displayForecast() {
+  let  forecastElement = document.querySelector("#forecast");
+  
+  let forecastHTMl = `<div class="row">`;
+  let days = ["Mon", "Tue", "Wed", "Thurs", "Fri"]
+days.forEach(function(day) {
+
+forecastHTMl =
+  forecastHTMl +
+  `
+        <div class="col-2"> <div class="day">${day}</div>
+          <div class="temperatures"><span class="tempHigh">85°</span></div>
+          <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAjdJREFUeNrtmsGtgzAMhjtCRmAERsgIHYFjjx2BERiBEToCI+TaG2+DbJBnKj8pD1Eaiv8AwpX+C1Ap/hzHjpNLCOFyZl0UgAJQAApAASgABaAAFIACODEA9C/83AypIrUkRwpv5Pib4dsCNp5cAMgIS3rMGPxJw3/t4QDQoEtSt8LwsTpJEFAAAONjNUM4HSIEOO7vJC8Mwa2FkHURZBDSM2KAWu4CwPP5LEgtyZMCy/OzIgKBgGA2BUAGliPDxxreldFM2EU4JAOgwRtSPeHdmj0/Z3z8fcEQasTCiATgEgxMURvNAkR2sOIA2MtBSH60KNbC4dAhAHhBACFDdrBiADj2A0DI7PA4AgB0dihWAaBBXUk92Hhkdqi+BsDGh8ySzg7tGgD9BgC88FrgvgKQIeY/ZgepxXB3m6EFm6bm7ADqQwDggSLUHQVA2KG6P50JQJ+a96UBuC08OwqRerOWGPftkUa7Oc/GrTBKo5b0mEivwzOLAlABjb8v6EY1CXVGgwCAampUqZ5NNP5fuS1aB6w87Zn1PG+XpStOKw3AIro5IONfM0e8EhRsZFxz7EYRACRmgQc0YPMAENrAdJk6UBgAKwojz/W/yQUgXgilARguS5dUd2Yir+foPzSQ7TAfifdLOjZvzhSzQID0A3gmuJTDzIQzRXg4IC9IvFsY68jzWxr/qgvQV2TsRJ1gwMXOIuW+JNWhjtx2DWCDE6fvAOhNUQWgABSAAlAACkABKAAFoABOp1+6Bd0LJ+BorgAAAABJRU5ErkJggg==" alt="" width="35"> </p>
+   <div class="temperatures">
+           <span class="tempLow">65°</span></div></div>
+   `;
+
+})
+
+  
+
+
+
+   forecastHTMl = forecastHTMl + `</div>`;
+
+     forecastElement.innerHTML = forecastHTMl;
+}
+
+
+
 function displayTemperature(response){
     console.log(response.data);
     let temperatureElement = document.querySelector("#real-Temp");
@@ -48,6 +79,8 @@ function handleSubmit(event) {
 }
 
 search("Miami");
+displayForecast();
 
 let form = document.querySelector("#engine");
 form.addEventListener("submit", handleSubmit);
+
